dcfaab4ffdff3fb998f11d53da2cabd426e214e1
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-10-07 21:40:05 -0700

    done the subtract function

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 /**
- * Function that adds two numbers
+ * Function that sums two numbers 'a' and 'b' together
  * @param {int} a - the first number
  * @param {int} b - the second number
  * @returns 
  */
 function add(a, b) {
     return a + b;
+}
+
+/**
+ * Subtracts 'b' amount from the initial 'a' amount
+ * @param {int} a - the first number to subtract from
+ * @param {int} b - the amount to subtract
+ * @returns 
+ */
+function substract(a, b) {
+    return a - b;
 }
